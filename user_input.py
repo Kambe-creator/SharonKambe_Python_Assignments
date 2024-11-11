aa299eec19b1aca407482bfedd49c3fd886910b9
Kambe-creator <sharonkambe@gmail.com>
2024-11-11 13:33:00 +0300

    Add files via upload
    Week 1 Assignement 1

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# Collect user information
+#Request for name
+name = input("Please enter your name: ")
+print(f"Name:{name}") 
+
+#Request for age
+age = input("Please enter your age: ")
+print(f"Age: {age}")
+
+#Request for location
+location = input("Please enter your location: ")
+print(f"Location: {location}")
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
